a97714debc9881fd21b521403c9f143ce0e1e37d
Alastair Cota <a1.cota.365@gmail.com>
2023-03-31 22:55:20 +0100

    Update vt100.py

--- sketches/Teletext/vt100.py
+++ sketches/Teletext/vt100.py
@@ -1,5 +1,5 @@
 reset = "\033[0m"
-bell = "\007"
+bell = "\007[0m"
 
 black = "\033[38;5;0m"
 white = "\033[38;5;15m"
@@ -8,6 +8,9 @@ red = "\033[38;5;1m"
 green = "\033[38;5;2m"
 yellow = "\033[38;5;3m"
 blue = "\033[38;5;4m"
+
+cyan = "\033[38;5;6m"
+purple = "\033[38;5;128m"
 
 bg_black = "\033[48;5;0m"
 bg_white = "\033[48;5;15m"
@@ -19,3 +22,6 @@ bg_blue = "\033[48;5;4m"
 
 def cursor(x, y):
     return "\033[" + str(y) + ";" + str(x) + "H"
+
+def resize(width = 100, height = 30):
+    return "\033[8;{};{}t".format(height, width)
